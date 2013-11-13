dab8299d2b8ea767f07753e88b084e301e6d8410
eriksteinebach <github@eriksteinebach.nl>
2013-11-13 20:55:03 +0100

    Tekst wijziging

--- MyContinuousDeployedApp/MyContinuousDeployedApp/Controllers/HomeController.cs
+++ MyContinuousDeployedApp/MyContinuousDeployedApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyContinuousDeployedApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Dit is een automatisch uitgerolde applicatie met TeamCity en Octopus Deploy";
 
             return View();
         }
