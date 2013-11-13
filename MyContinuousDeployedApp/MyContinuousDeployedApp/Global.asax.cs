ae71f6deb16b5bb9049143558640a73df1f0f46e
eriksteinebach <github@eriksteinebach.nl>
2013-11-13 20:39:23 +0100

    Nu wel werkende website

--- MyContinuousDeployedApp/MyContinuousDeployedApp/Global.asax.cs
+++ MyContinuousDeployedApp/MyContinuousDeployedApp/Global.asax.cs
@@ -22,6 +22,7 @@ namespace MyContinuousDeployedApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
--- MyContinuousDeployedApp/MyContinuousDeployedApp/Properties/AssemblyInfo.cs
+++ MyContinuousDeployedApp/MyContinuousDeployedApp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a1554db-ba1f-4ab9-8524-cd70db24e1ff")]
+[assembly: Guid("17f363c7-6528-485b-9d66-e7c35ed44b63")]
 
 // Version information for an assembly consists of the following four values:
 //
